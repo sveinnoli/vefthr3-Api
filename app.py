c8e0b280b5a0da0e4001a56053491e064b0ebb0f
sveinnoli <46562494+sveinnoli@users.noreply.github.com>
2020-02-12 10:41:01 +0000

    inital heroku setup

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import urllib.request, json # þurfum þennan til að sækja JSON object yfir netið og til að höndla json í py skránni
+app = Flask(__name__)
+
+
+# rútínan hérna fyrir neðan fer á url, sækir JSON object og setur í url breytuna
+# json.loads hendir JSON obect yfir í dictionary sem við vinnum með í .py skrá
+with urllib.request.urlopen("http://apis.is/petrol") as url:
+    data = json.loads(url.read().decode())
+
+@app.route('/')
+def index():
+	# print(type(data)) debug, sjáum hvaða týpa data er = dict
+	return data
+
+if __name__ == "__main__":
+	app.run()
