db2548a220aa13460cf367fcea74e105a683f0df
sveinnoli <46562494+sveinnoli@users.noreply.github.com>
2020-02-19 11:40:55 +0000

    indexed dictionary

--- app.py
+++ app.py
@@ -9,13 +9,29 @@ with urllib.request.urlopen("http://apis.is/petrol") as url:
     data = json.loads(url.read().decode())
 
 #----- Data -----
-fyrirtaeki = []
-for item in range(len(data["results"])):
-    company = data["results"][item].get("company").capitalize()
-    if company in fyrirtaeki:
-        pass
-    else:
-        fyrirtaeki.append(company)
+def results(dict_value):
+    item_list = []
+    for item in range(len(data["results"])):
+        dict_item = data["results"][item].get(dict_value)
+        if dict_value == "company":
+            if dict_item in item_list:
+                pass
+            else:
+                item_list.append(dict_item)
+        else:
+            item_list.append(dict_item)
+    return item_list
+
+data_keys = list(data["results"][0].keys())
+price = results(data_keys[0])
+price_discount = results(data_keys[1])
+company = results(data_keys[2])
+diesel = results(data_keys[3])
+diesel_discount = results(data_keys[4])
+geo = results(data_keys[5])
+key = results(data_keys[6])
+name = results(data_keys[7])
+    
 
 @app.route('/')
 def index():
--- test.py
+++ test.py
@@ -24,9 +24,46 @@ with urllib.request.urlopen("http://apis.is/petrol") as url:
 
     #print("item number:", item, "\n", data["results"][item], "\n") # Iterates through dict -> list
     #verd = data["results"][item].get("bensin95") #Gets price from results
+def results(dict_value):
+    item_list = []
+    for item in range(len(data["results"])):
+        dict_item = data["results"][item].get(dict_value)
+        if dict_value == "company":
+            if dict_item in item_list:
+                pass
+            else:
+                item_list.append(dict_item)
+        else:
+            item_list.append(dict_item)
+    return item_list
 
+data_keys = list(data["results"][0].keys())
+price = results(data_keys[0])
+price_discount = results(data_keys[1])
+company = results(data_keys[2])
+diesel = results(data_keys[3])
+diesel_discount = results(data_keys[4])
+geo = results(data_keys[5])
+key = results(data_keys[6])
+name = results(data_keys[7])
 
+print(company)
+    
+#print([data["results"][1].keys()])
 
+data_keys = list(data["results"][0].keys())
+
+#"bensin95": 233.9, 
+#      "bensin95_discount": 228.9, 
+#      "company": "Atlantsol\u00eda", 
+#      "diesel": 226.2, 
+#      "diesel_discount": 221.2, 
+#      "geo": {
+#        "lat": 65.69913, 
+#        "lon": -18.135231
+#      }, 
+#      "key": "ao_000", 
+#      "name": "Baldursnes Akureyri"
 #---------------------------
 # Dictionary Testing
 #---------------------------
