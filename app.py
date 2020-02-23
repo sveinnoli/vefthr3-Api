5aa6ddb33434bf0e8f63f8d3abeb7ab2c39dfdcb
sveinnoli <46562494+sveinnoli@users.noreply.github.com>
2020-02-23 18:28:28 +0000

    added frontpage, adding tables for locations

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
 from flask import Flask, render_template
-import urllib.request, json # þurfum þennan til að sækja JSON object yfir netið og til að höndla json í py skránni
+import urllib.request, json #Gets json objects
 app = Flask(__name__)
-
-# rútínan hérna fyrir neðan fer á url, sækir JSON object og setur í url breytuna
-# json.loads hendir JSON obect yfir í dictionary sem við vinnum með í .py skrá
-# Data er Dictionary sem inniheldur lista "results" sem inniheldur dictionary, þar eru gögnin sem eru þörf á.
 with urllib.request.urlopen("http://apis.is/petrol") as url:
     data = json.loads(url.read().decode())
 
-#----- Data -----
-def results(dict_value):
+#----- Appends data from dictionary into a list -----
+def list_results(dict_value, sorting=False):
     item_list = []
     for item in range(len(data["results"])):
         dict_item = data["results"][item].get(dict_value)
-        if dict_value == "company":
+        if dict_value == "company" and sorting==True: 
             if dict_item in item_list:
                 pass
             else:
@@ -22,21 +18,36 @@ def results(dict_value):
             item_list.append(dict_item)
     return item_list
 
-data_keys = list(data["results"][0].keys())
-price = results(data_keys[0])
-price_discount = results(data_keys[1])
-company = results(data_keys[2])
-diesel = results(data_keys[3])
-diesel_discount = results(data_keys[4])
-geo = results(data_keys[5])
-key = results(data_keys[6])
-name = results(data_keys[7])
-    
+# Checks to make sure that company is still in dataset, if it's not present the logo will not appear on the Homepage
+def CompanyExists(company, company_image):
+    image_list = {}
+    for x in range(len(company)):
+        for key, value in company_image.items():
+            if key == company[x]:
+                print("Not in list:",company[x],key)
+                image_list[company[x]] = value
+            else:
+                pass
+    return image_list
+
+
+# 0=price, 1=price_discount, 2=company, 3=diesel, 4=diesel_discount, 5=geo, 6=key, 7=name:
+data_keys = list(data["results"][0].keys()) 
+company = list_results(data_keys[2], True) 
+company_image = {"Atlantsolía":"/static/images/Atlantsolía.png", "Costco Iceland": "/static/images/Costco Iceland.png", "Dælan":"/static/images/Dælan.png", "N1":"/static/images/N1.png", "ÓB": "/static/images/ÓB.png","Olís":"/static/images/Olís.png","Orkan":"/static/images/Orkan.png" }
+
 
 @app.route('/')
 def index():
-	# print(type(data)) debug, sjáum hvaða týpa data er = dict
-	return data
+	return render_template("gasvakt.html", company=CompanyExists(company,company_image))
+
+@app.route('/company/<gasCompany>')
+def station(gasCompany):
+    return render_template("station.html",gasCompany=gasCompany)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == "__main__":
 	app.run(debug=True)
--- test.py
+++ test.py
@@ -24,46 +24,11 @@ with urllib.request.urlopen("http://apis.is/petrol") as url:
 
     #print("item number:", item, "\n", data["results"][item], "\n") # Iterates through dict -> list
     #verd = data["results"][item].get("bensin95") #Gets price from results
-def results(dict_value):
-    item_list = []
-    for item in range(len(data["results"])):
-        dict_item = data["results"][item].get(dict_value)
-        if dict_value == "company":
-            if dict_item in item_list:
-                pass
-            else:
-                item_list.append(dict_item)
-        else:
-            item_list.append(dict_item)
-    return item_list
 
-data_keys = list(data["results"][0].keys())
-price = results(data_keys[0])
-price_discount = results(data_keys[1])
-company = results(data_keys[2])
-diesel = results(data_keys[3])
-diesel_discount = results(data_keys[4])
-geo = results(data_keys[5])
-key = results(data_keys[6])
-name = results(data_keys[7])
 
-print(company)
-    
 #print([data["results"][1].keys()])
 
-data_keys = list(data["results"][0].keys())
 
-#"bensin95": 233.9, 
-#      "bensin95_discount": 228.9, 
-#      "company": "Atlantsol\u00eda", 
-#      "diesel": 226.2, 
-#      "diesel_discount": 221.2, 
-#      "geo": {
-#        "lat": 65.69913, 
-#        "lon": -18.135231
-#      }, 
-#      "key": "ao_000", 
-#      "name": "Baldursnes Akureyri"
 #---------------------------
 # Dictionary Testing
 #---------------------------
@@ -75,3 +40,13 @@ data_keys = list(data["results"][0].keys())
 #
 #for key, value in thisdict.items():
 #	print(key, value)
+
+image_list = ["/static/images/Atlantsolía.png", "/static/images/Costco Iceland.png", "/static/images/Dælan.png", "/static/images/N1.png", "/static/images/ÓB.png", "/static/images/Olís.png", "/static/images/Orkan.png"]
+
+image_dict = {"Atlantsolía":"/static/images/Atlantsolía.png", "Costco Iceland": "/static/images/Costco Iceland.png", "Dælan":"/static/images/Dælan.png", "N1":"/static/images/N1.png", "Olís":"/static/images/Olís.png","Orkan":"/static/images/Orkan.png" }
+
+company_images={"Atlantsolía":"lol", "Olís":"195"}
+for key,value in image_dict.items():
+    print(key)
+
+company_images.update(Atlantsolía = "image")
