6cf3980255121ce9ba2772871211f6f33df400ad
sveinnoli <46562494+sveinnoli@users.noreply.github.com>
2020-02-24 16:58:04 +0000

    missed

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import urllib.request, json, time, datetime, # þurfum þennan til að sækja JSON object yfir netið og til að höndla json í py skránni
+import urllib.request, json # þurfum þennan til að sækja JSON object yfir netið og til að höndla json í py skránni
 
 with urllib.request.urlopen("http://apis.is/petrol") as url:
     data = json.loads(url.read().decode())
